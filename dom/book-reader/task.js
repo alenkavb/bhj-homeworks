73fa3947ac72b7bd64f583fca2cf8ba973715ae7
Елена Бухалова <alenkavb@gmail.com>
2022-07-25 21:55:36 +0700

    solid1

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,22 +1,19 @@
 const fontSizes = document.querySelectorAll('.font-size');
-console.log(fontSizes);
 
 fontSizes.forEach((fontSize) => {
     fontSize.addEventListener("click", function(event) {
         const fontSizeActiv = document.querySelector('.font-size_active');
-    fontSizeActiv.classList.remove('font-size_active');
+        fontSizeActiv.classList.remove('font-size_active');
 
-    this.classList.add('font-size_active');
-    let data = this.dataset.size;
+        this.classList.add('font-size_active');
+        let data = this.dataset.size;
     
-    const book = document.querySelector('.book');
-    console.log(data);
-    book.classList.remove('book_fs-big', 'book_fs-small');
-    if (data !== undefined) {
-        book.classList.add('book_fs-' + data);
-        console.log(book);
-    };
-    event.preventDefault();
-})
+        const book = document.querySelector('.book');
+        book.classList.remove('book_fs-big', 'book_fs-small');
+        if (data !== undefined) {
+            book.classList.add('book_fs-' + data);
+        };
+        event.preventDefault();
+    })
 })
 
