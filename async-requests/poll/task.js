5952f83d1e960b7ce37b4a9a4e950a955029fb57
Елена Бухалова <alenkavb@gmail.com>
2022-08-08 15:48:23 +0700

    solution

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,3 +1,5 @@
+const divAnswers = document.getElementById('poll__answers');
+
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
 xhr.send();
@@ -5,13 +7,24 @@ xhr.send();
 xhr.onreadystatechange = function() {
   if (xhr.readyState === xhr.DONE) {
     let data = JSON.parse(xhr.responseText);
-    console.dir(data);
-    console.log(data.data.title);
-   
+    
     const divTitle = document.getElementById('poll__title');
-    divTitle.textContent = data.data.title;
+    divTitle.innerText = data.data.title;
+    
+    data.data.answers.forEach(element => {
+      const button = document.createElement('button');
+      button.classList.add('poll__answer');
+      button.innerText = element;
 
-    const divAnswers = document.getElementById('poll__answers')
+      divAnswers.appendChild(button);
+    }); 
   }
 }
+
+divAnswers.addEventListener('click', (event) => {
+  if (event.target.closest('.poll__answer')) {
+    alert('Спасибо, ваш голос засчитан!');
+  }
+})
+
 
