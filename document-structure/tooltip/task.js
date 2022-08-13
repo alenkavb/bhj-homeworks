898e57e9bef4305992500e5fe9b01d8f7d693c85
Елена Бухалова <alenkavb@gmail.com>
2022-08-13 16:49:38 +0700

    solution1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,24 +1,34 @@
-const wrapper = document.querySelector('.wrapper');
+const elems = document.querySelectorAll(".has-tooltip");
+elems.forEach(elem => {
+   const div = document.createElement("div");
+   div.classList.add("tooltip");
+   elem.after(div); 
 
-wrapper.addEventListener('click', function(event) {
-  event.preventDefault();
-  const elem = event.target.closest('.has-tooltip');
-  
-  if (elem) {
-    const position = elem.getBoundingClientRect();
-    const div = document.createElement('div');
-  
-    div.className = 'tooltip';
-    elem.after(div);
-
-    elem.nextSibling.setAttribute('style', `left: ${position.left}px; top: ${position.top + 20}px`)
-    elem.nextSibling.innerHTML = elem.title;
-    if (document.querySelector('.tooltip_active')) {
-      (document.querySelector('.tooltip_active')).classList.remove('tooltip_active') 
+   elem.onclick = () => {
+      const position = elem.getBoundingClientRect();
+      const prompt = elem.nextElementSibling;
+      prompt.innerHTML = elem.title;
+      prompt.setAttribute("style", `left: ${position.left}px; top: ${position.top + 20}px;`);
+    
+    
+    if (prompt.classList.contains("tooltip_active")) {
+      prompt.classList.remove("tooltip_active");
+    } else if (document.querySelector(".tooltip_active")){
+      document.querySelector(".tooltip_active").classList.remove("tooltip_active");
+      prompt.classList.add("tooltip_active");
+    } else {
+      prompt.classList.add("tooltip_active");
     }
-    elem.nextSibling.classList.add('tooltip_active');
-  } else {
-    (document.querySelector('.tooltip_active')).classList.remove('tooltip_active');
+    return false;
+   };
+  });  
+   
+window.onscroll = function(e) {
+  const prompt = document.querySelector('.tooltip_active');
+  if (prompt) {
+    const elem = prompt.previousElementSibling;
+    const position = elem.getBoundingClientRect();
+    prompt.setAttribute("style", `left: ${position.left}px; top: ${position.top + 20}px;`);
   };
-})
+}
 
